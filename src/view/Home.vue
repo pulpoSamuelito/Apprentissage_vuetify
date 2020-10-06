7d76e2ff356ec93d555cf3ffe0ad5ed96984644d
pulpoSamuelito <samuelntouba72@gmail.com>
2020-10-06 02:22:40 +0100

    first commit

---
+++ src/view/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+          <v-row>
+            
+          </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    step: 1
+  }),
+  props: {
+      source: String
+  }
+};
+</script>
